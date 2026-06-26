4ea09521f431ce8362562c02a5d6e70d4aebeff3
evanstef <evangundala@gmail.com>
2026-06-26 23:03:31 +0700

    feat: w

--- multienv-app/environments/staging/main.tf
+++ multienv-app/environments/staging/main.tf
@@ -15,6 +15,15 @@ variable "db_password" {
 
 provider "aws" {
   region = "ap-southeast-1"
+
+  # Tag default → otomatis nempel ke SEMUA resource di env ini (DRY)
+  default_tags {
+    tags = {
+      CostCenter  = "Engineering"
+      Owner       = "Evan"
+      Environment = "staging"
+    }
+  }
 }
 
 # Bikin jaringan (VPC + subnet + IGW + NAT) buat env ini
@@ -29,28 +38,28 @@ module "eks" {
   subnet_ids  = module.network.private_subnet_ids
 }
 
-# module "app_stack" {
-#   source         = "../../modules/app-stack"
-#   environment    = "staging"
-#   db_password    = var.db_password
-#   instance_type  = "t3.micro"
-#   app_domain     = "app-staging.evnxc.web.id"
-#   grafana_domain = "grafana-staging.evnxc.web.id"
+module "app_stack" {
+  source         = "../../modules/app-stack"
+  environment    = "staging"
+  db_password    = var.db_password
+  instance_type  = "t3.micro"
+  app_domain     = "app-staging.evnxc.web.id"
+  grafana_domain = "grafana-staging.evnxc.web.id"
 
-#   # Oper hasil module network → biar app masuk ke VPC ini
-#   vpc_id               = module.network.vpc_id
-#   public_subnet_id     = module.network.public_subnet_id
-#   db_subnet_group_name = module.network.db_subnet_group_name
-# }
+  # Oper hasil module network → biar app masuk ke VPC ini
+  vpc_id               = module.network.vpc_id
+  public_subnet_id     = module.network.public_subnet_id
+  db_subnet_group_name = module.network.db_subnet_group_name
+}
 
-# output "server_ip" {
-#   value = module.app_stack.server_ip
-# }
+output "server_ip" {
+  value = module.app_stack.server_ip
+}
 
-# output "ssh_command" {
-#   value = module.app_stack.ssh_command
-# }
+output "ssh_command" {
+  value = module.app_stack.ssh_command
+}
 
-# output "db_endpoint" {
-#   value = module.app_stack.db_endpoint
-# }
+output "db_endpoint" {
+  value = module.app_stack.db_endpoint
+}
